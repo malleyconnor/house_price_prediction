ee95e2322aae39d8016d3661634191853314e9a9
Connor Malley <connormalley98@gmail.com>
2020-09-30 19:07:38 -0400

    Splits dataset into features and labels

--- price_predict.py
+++ price_predict.py
@@ -1,8 +1,14 @@
 import numpy as numpy
+import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn import preprocessing
 
 
-if __name__ == "__main__":
-    print("Hello world!")
+if __name__ == '__main__':
+    inputData = pd.read_csv('./data/kc_house_data.csv')
+    inputLabels   = inputData['price']
+    inputFeatures = inputData.drop('price', inplace=False, axis=1)
+
+    print(inputFeatures.head)
+    print(inputLabels.head)
