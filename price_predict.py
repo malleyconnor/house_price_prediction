b4ac5256dc6382e8e56148906c9968c23d3cd0df
Connor Malley <connormalley98@gmail.com>
2020-10-28 16:10:01 -0400

    Added some comments, feature ranking w/ correlation and RF, and preliminary RF test

--- price_predict.py
+++ price_predict.py
@@ -5,6 +5,8 @@ import sklearn
 import statistics
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error, median_absolute_error
 from sklearn.model_selection import train_test_split
 import sys, getopt
 import os
@@ -37,40 +39,51 @@ typeEnum = {
     'continuous'    : 3
 }
 
-def normalize_data(X, Y):
+# Normalizes data columns between 0 and 1
+def normalize_data(X_train, X_test, Y_train, Y_test, normalize_labels=True):
     # Normalizing features between 0 and 1
     mmScaler = preprocessing.MinMaxScaler()
-    for column in X.columns:
-        X[column] = mmScaler.fit_transform(X[column].to_numpy().reshape(-1, 1))
+    mmScaler.fit(X_train)
+    X_train = pd.DataFrame(mmScaler.transform(X_train), index=X_train.index, columns=X_train.columns)
+    X_test  = pd.DataFrame(mmScaler.transform(X_test), index=X_test.index, columns=X_test.columns)
+
 
     # Normalizing labels as well
-    Y = mmScaler.fit_transform(Y)
-    Y_temp = pd.DataFrame()
-    Y_temp['price'] = Y[:,0]
-    Y = Y_temp
+    if (normalize_labels):
+        mmScaler.fit(Y_train)
+        Y_train = pd.DataFrame(mmScaler.transform(Y_train), index=X_train.index, columns=['price'])
+        Y_test  = pd.DataFrame(mmScaler.transform(Y_test), index=X_test.index, columns=['price'])
 
-    return X, Y
+    return X_train, X_test, Y_train, Y_test
 
 
-
+# Preprocesses data by normalizing, dropping specified features, and splitting into train/test sets
 def preprocess_data(path, drop_features=['date'], label='price',
-    save_dir=None):
+    save_dir=None, test_size=0.2, normalize_labels=True):
     # Splitting data into features/labels
     X = pd.read_csv(path)
-    Y  = pd.DataFrame(X[label])
+    Y = pd.DataFrame(X[label])
 
     # Dropping label and irrelevant features from X
     X.drop(label, inplace=True, axis=1)
     X.drop(drop_features, inplace=True, axis=1)
 
-    X, Y = normalize_data(X, Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, 
+    shuffle=True, random_state=47)
+    min_norm = np.min(Y_train)
+    max_norm = np.max(Y_train)
+    X_train, X_test, Y_train, Y_test = normalize_data(X_train, X_test, Y_train, Y_test, 
+    normalize_labels)
     
     # Exporting normalized data to CSV
     if (save_dir != None):    
-        X.to_csv('%s/X.csv' % (save_dir))
-        Y.to_csv('%s/Y.csv' % (save_dir))
+        X_train.to_csv('%s/X_train.csv' % (save_dir))
+        Y_train.to_csv('%s/Y_train.csv' % (save_dir))
 
-    return X, Y
+        X_test.to_csv('%s/X_test.csv' % (save_dir))
+        Y_test.to_csv('%s/Y_test.csv' % (save_dir))
+
+    return X_train, X_test, Y_train, Y_test, min_norm, max_norm
 
 
 
@@ -108,12 +121,18 @@ def get_correlation(X, Y):
     return covariance[0][1] / (std_X*std_Y)
 
 # Gets list of correlations, sorted in reverse order by magnitude of correlation for each feature within a dataframe
-def get_correlations(X, Y):
+def get_correlations(X, Y, disp=False):
     correlations = []
     for i, column in enumerate(X.columns):
         correlations.append((column, get_correlation(X[column], Y[Y.columns[0]])))
 
     correlations = sorted(correlations, key=lambda tup: abs(tup[1]), reverse=True)
+
+    if (disp):
+        print('Correlations of Features reverse sorted by magnitude:')
+        for i in range(len(correlations)):
+            print(correlations[i])
+
     return correlations
 
 
@@ -152,18 +171,37 @@ def get_feature_stats(X):
         except statistics.StatisticsError:
             mode = -1
         # TODO: Add frequency of mode
-        # TODO: Add impurity / entropy measures
 
     return stats
 
+# Returns a label value to its unnormalized price
+def unnormalize_val(y, min_val, max_val):
+    return y * (max_val - min_val) + min_val
 
+def unnormalize_arr(Y, min_val, max_val):
+    for i in range(len(Y)):
+        Y[i] = unnormalize_val(Y[i], min_val, max_val)
 
-if __name__ == '__main__':
+    return Y
+
+# Plots a population histogram split by lat/long as specified in bins
+def plot_lat_long_hist(X, bins=(5,5), save_path='./figures/latlong_hist.png'):
+    # TODO: Bin lat/long data into a 2-D grid and enumerate
+    # or just remove in favor of zip code
+    plt.hist2d(X['long'], X['lat'], bins=(5,5))
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title('Lat/Long Counts, Bins = %s' % (str(bins)))
+    plt.colorbar()
+    plt.savefig(save_path, dpi=300)
+    plt.clf()
+
+# Command-line Argument handler
+def handle_cl_args():
     # Default input variables
     savePlots = False
     plotDir = './figures'
 
-    # Command-line Option handler
     opts, args = getopt.getopt(sys.argv[1:], 'hp', ['help', 'plot='])
     for opt, arg in opts:
         if opt in ('-h', '--help'):
@@ -174,11 +212,30 @@ if __name__ == '__main__':
                 plotDir = arg
             savePlots = True
 
+    return (savePlots, plotDir)
 
-    # Normalize on [0,1] and remove selected features    
-    X, Y = preprocess_data('./data/kc_house_data.csv', 
-           drop_features=['date', 'id'], save_dir='./data')
+# Ranks the input features using a random forest algorithm (MSE)
+def rf_rank(X, Y, n_estimators=100, disp=False):
+    rf = RandomForestRegressor(n_estimators=100, n_jobs=-1)
+    rf.fit(X_train, Y_train['price'])
+    feature_importances = zip(X_train.columns, rf.feature_importances_)
+    feature_importances = sorted(feature_importances, key=lambda tup: abs(tup[1]), reverse=True)
 
+    if (disp):
+        print('\nTop 10 Features reverse sorted by importance from random forest')
+        for feature in feature_importances:
+            print(feature)
+    
+    return feature_importances
+
+if __name__ == '__main__':
+    savePlots, plotDir = handle_cl_args()
+
+    # Initial Preprocessing of data
+    test_size = 0.2
+    X_train, X_test, Y_train, Y_test, min_norm, max_norm =\
+    preprocess_data('./data/kc_house_data.csv', drop_features=['date', 'id'], 
+    save_dir='./data', test_size=test_size, normalize_labels=False)
 
     # Plots histograms
     if (savePlots):
@@ -187,25 +244,35 @@ if __name__ == '__main__':
 
         plot_feature_histograms(X, save_dir=plotDir)
         plot_feature_correlation(X, Y, save_dir=plotDir+"/correlation")
+        plot_lat_long_hist(X)
 
+
+    # TODO: Keep either zipcode or lat/long, possibly redundant 
+    # (if keeping lat/long, then maybe bin it. Higher priced homes are likely to be on north west)
 
     # Ranks the correlations of each variable
-    correlations = get_correlations(X, Y)
-    print('Correlations of Features reverse sorted by magnitude:')
-    for i in range(len(correlations)):
-        print(correlations[i])
-
-
+    correlations = get_correlations(X_train, Y_train, disp=True)
 
     # Ranking features using random forest (w/ MSE)
-    rf = RandomForestRegressor(n_estimators=1000, max_depth=10, n_jobs=-1)
-    rf.fit(X, Y[Y.columns[0]])
-    feature_importances = zip(X.columns, rf.feature_importances_)
-    feature_importances = sorted(feature_importances, key=lambda tup: abs(tup[1]), reverse=True)
-    print('\nTop 10 Features reverse sorted by importance from random forest')
-    for feature in feature_importances:
-        print(feature)
+    # TODO: Check as a function of n_estimators and max_depth, and see how it changes feature ranks or results
+    feature_importances = rf_rank(X_train, Y_train, n_estimators=100, disp=True)
+
+    # TODO: Try mRMR for removing redundant features (see if it agrees with random forest)
 
 
-    # TODO: Bin lat/long data into a 2-D grid and enumerate
-    # TODO: Try mRMR for ranking features (see if it agrees with random forest)
+    # Training random forest regressors using restricted number of top features
+    # TODO: Try exhaustive/randomized grid search with n_features, n_estimators, max_depth
+    num_features = 7
+    n_estimators = 100
+    print(max_norm)
+    feature_list = [feature[0] for feature in feature_importances[0:num_features]]
+    max_depth = num_features
+    rf = RandomForestRegressor(n_estimators=n_estimators, n_jobs=-1, max_depth=max_depth)
+    rf.fit(X_train[feature_list], Y_train['price'])
+    Y_pred = rf.predict(X_test[feature_list])
+    rf_error = mean_absolute_error(Y_test, Y_pred)
+    print('MAE of Random Forest: %f' % (rf_error))
+
+    # TODO: Use weights of feature importances for KNN
+
+
